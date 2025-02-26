38cafb627166b4addf5fc2464c090784248469c3
qqaazz800624 <qqaazz800624@gmail.com>
2025-02-26 13:56:24 +0800

    Update Problem 5: Friend Cover

--- hw0/P5/5.c
+++ hw0/P5/5.c
@@ -1,78 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
 
-// We assume n ≤ 20 so that 1<<n is at most about 1 million.
-int main(void) {
+#define MAX_N 20
+#define INF 100
+
+int edges[MAX_N][MAX_N]; 
+int bestCover[MAX_N], bestSize = INF;
+
+int isValidCover(int subset, int n);
+void updateBestCover(int subset, int n);
+void findMinimumVertexCover(int n);
+
+int main(){
     int n, m;
-    if (scanf("%d %d", &n, &m) != 2)
-        return 1;
-    
-    // Read the m friendship pairs.
-    // Maximum m is n(n-1)/2, so we use an array large enough.
-    int friendPairs[210][2]; // a bit more than needed
-    for (int i = 0; i < m; i++){
-        scanf("%d %d", &friendPairs[i][0], &friendPairs[i][1]);
+    int i, j;
+    scanf("%d %d", &n, &m);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            edges[i][j] = 0;
+        }
     }
-    
-    int bestMask = -1;
-    int bestSize = n + 1; // initial "infinite" size
-    int total = 1 << n;
-    
-    // Iterate over all possible subsets represented by bitmask [0, 1<<n)
-    for (int mask = 0; mask < total; mask++){
-        int count = __builtin_popcount(mask);
-        // Skip if already larger than current best.
-        if (count > bestSize) continue;
-        
-        bool valid = true;
-        // For each friendship pair, check that at least one endpoint is included.
-        for (int i = 0; i < m; i++){
-            int u = friendPairs[i][0];
-            int v = friendPairs[i][1];
-            // If neither u nor v is in the subset, then this is not a cover.
-            if (((mask >> u) & 1) == 0 && ((mask >> v) & 1) == 0) {
-                valid = false;
+
+    int u, v;
+    for (i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        edges[u][v] = edges[v][u] = 1;
+    }
+
+    findMinimumVertexCover(n);
+
+    for (i = 0; i < bestSize; i++){
+        printf("%d\n", bestCover[i]);
+    }
+
+    return 0;
+}
+
+int isValidCover(int subset, int n){
+    int u, v;
+    for (u = 0; u < n; u++){
+        for (v = u + 1; v < n; v++){
+            if (edges[u][v]){
+                if (!((subset & (1 << u)) || (subset & (1 << v)))){
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+void updateBestCover(int subset, int n){
+    int count = 0;
+    int tempCover[MAX_N];
+    int i, j;
+
+    for (i = 0; i < n; i++){
+        if (subset & (1 << i)){
+            tempCover[count++] = i;
+        }
+    }
+
+    if (count < bestSize){
+        bestSize = count;
+        for (i = 0; i < count; i++){
+            bestCover[i] = tempCover[i];
+        }
+    } else if (count == bestSize){
+        for (i = 0; i < count; i++){
+            if (tempCover[i] < bestCover[i]){
+                for (j = 0; j < count; j++){
+                    bestCover[j] = tempCover[j];
+                }
+                break;
+            } else if (tempCover[i] > bestCover[i]){
                 break;
             }
         }
-        if (!valid)
-            continue;
-        
-        // If valid, then we have a candidate cover.
-        if (count < bestSize) {
-            bestSize = count;
-            bestMask = mask;
-        } else if (count == bestSize) {
-            // If candidate has the same size as the best found so far,
-            // choose the lexicographically smallest cover.
-            // Lexicographical order means that if you list the indices (in ascending order)
-            // from the subset, the one with the smallest number at the first differing position is chosen.
-            bool candidateBetter = false;
-            for (int i = 0; i < n; i++){
-                int bitCandidate = (mask >> i) & 1;
-                int bitBest = (bestMask >> i) & 1;
-                if (bitCandidate != bitBest) {
-                    // If candidate has a '1' at a lower index where bestMask has '0',
-                    // then candidate's sorted list starts with that index, which is lexicographically smaller.
-                    if (bitCandidate == 1 && bitBest == 0) {
-                        candidateBetter = true;
-                    }
-                    break;
-                }
-            }
-            if (candidateBetter) {
-                bestMask = mask;
-            }
+    }
+}
+
+void findMinimumVertexCover(int n){
+    int totalSubsets = (1 << n);
+    int subset;
+    for (subset = 0; subset < totalSubsets; subset++) {
+        if (isValidCover(subset, n)) {
+            updateBestCover(subset, n);
         }
     }
-    
-    // Output the indices in the best cover (in increasing order), one per line.
-    // (Indices are 0-indexed.)
-    for (int i = 0; i < n; i++){
-        if ((bestMask >> i) & 1)
-            printf("%d\n", i);
-    }
-    
-    return 0;
 }
--- hw0/P5/test.c
+++ hw0/P5/test.c
@@ -1,69 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
+
+#define MAX_N 20
+#define INF 100
+
+int edges[MAX_N][MAX_N]; 
+int bestCover[MAX_N], bestSize = INF;
+
+int isValidCover(int subset, int n);
+void updateBestCover(int subset, int n);
+void findMinimumVertexCover(int n);
 
 int main(){
-
     int n, m;
+    int i, j;
     scanf("%d %d", &n, &m);
 
-    int friendPairs[200][2];
-    int i;
-    for(i = 0; i < m; i++){
-        scanf("%d %d", &friendPairs[i][0], &friendPairs[i][1]);
-    }
-
-    int bestMask = -1;
-    int bestSize = n + 1;
-    int total = 1 << n;
-
-    int mask;
-    for (mask = 0; mask < total; mask++){
-        int count;
-        count = __builtin_popcount(mask);
-        if (count > bestSize){
-            continue;
-        }
-
-        bool valid = true;
-        for(i = 0; i < m; i++){
-            int u = friendPairs[i][0];
-            int v = friendPairs[i][1];
-            if (((mask >> u) & 1) == 0 && ((mask >> v) & 1) == 0){
-                valid = false;
-                break;
-            }
-        }
-        if (!valid){
-            continue;
-        }
-
-        if (count < bestSize){
-            bestSize = count;
-            bestMask = mask;
-        } else if (count == bestSize){
-            bool candidateBetter = false;
-            for (i = 0; i < n; i++){
-                int bitCandidate = (mask >> i) & i;
-                int bitBest = (bestMask >> i) & i;
-                if (bitCandidate != bitBest){
-                    if (bitCandidate == 1 && bitBest == 0){
-                        candidateBetter = true;
-                    }
-                }
-                break;
-            }
-            if (candidateBetter){
-                bestMask = mask;
-            }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            edges[i][j] = 0;
         }
     }
 
-    for (i = 0; i < n; i++){
-        if ((bestMask >> i) & 1){
-            printf("%d\n", i);
-        }
+    int u, v;
+    for (i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        edges[u][v] = edges[v][u] = 1;
+    }
+
+    findMinimumVertexCover(n);
+
+    for (i = 0; i < bestSize; i++){
+        printf("%d\n", bestCover[i]);
     }
 
     return 0;
+}
+
+int isValidCover(int subset, int n){
+    int u, v;
+    for (u = 0; u < n; u++){
+        for (v = u + 1; v < n; v++){
+            if (edges[u][v]){
+                if (!((subset & (1 << u)) || (subset & (1 << v)))){
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+void updateBestCover(int subset, int n){
+    int count = 0;
+    int tempCover[MAX_N];
+    int i, j;
+
+    for (i = 0; i < n; i++){
+        if (subset & (1 << i)){
+            tempCover[count++] = i;
+        }
+    }
+
+    if (count < bestSize){
+        bestSize = count;
+        for (i = 0; i < count; i++){
+            bestCover[i] = tempCover[i];
+        }
+    } else if (count == bestSize){
+        for (i = 0; i < count; i++){
+            if (tempCover[i] < bestCover[i]){
+                for (j = 0; j < count; j++){
+                    bestCover[j] = tempCover[j];
+                }
+                break;
+            } else if (tempCover[i] > bestCover[i]){
+                break;
+            }
+        }
+    }
+}
+
+void findMinimumVertexCover(int n){
+    int totalSubsets = (1 << n);
+    int subset;
+    for (subset = 0; subset < totalSubsets; subset++) {
+        if (isValidCover(subset, n)) {
+            updateBestCover(subset, n);
+        }
+    }
 }
