a4ac946420c974fe136bcaaa39ab9af9a25df5a3
qqaazz800624 <qqaazz800624@gmail.com>
2025-05-13 12:37:54 +0800

    test script

---
+++ hw3/P4/test.sh
@@ -0,0 +1,60 @@
+./14 < 0-sample-01.in > my_0_sample_01.out
+diff my_0_sample_01.out 0-sample-01.out
+./14 < 0-sample-02.in > my_0_sample_02.out
+diff my_0_sample_02.out 0-sample-02.out
+./14 < 0-sample-03.in > my_0_sample_03.out
+diff my_0_sample_03.out 0-sample-03.out
+
+./14 < 1-public-01.in > my_1_public_01.out
+diff my_1_public_01.out 1-public-01.out
+./14 < 1-public-02.in > my_1_public_02.out
+diff my_1_public_02.out 1-public-02.out
+./14 < 1-public-03.in > my_1_public_03.out
+diff my_1_public_03.out 1-public-03.out
+./14 < 1-public-04.in > my_1_public_04.out
+diff my_1_public_04.out 1-public-04.out
+./14 < 1-public-05.in > my_1_public_05.out
+diff my_1_public_05.out 1-public-05.out
+./14 < 1-public-06.in > my_1_public_06.out
+diff my_1_public_06.out 1-public-06.out
+./14 < 1-public-07.in > my_1_public_07.out
+diff my_1_public_07.out 1-public-07.out
+./14 < 1-public-08.in > my_1_public_08.out
+diff my_1_public_08.out 1-public-08.out
+
+./14 < 2-public-01.in > my_2_public_01.out
+diff my_2_public_01.out 2-public-01.out
+./14 < 2-public-02.in > my_2_public_02.out
+diff my_2_public_02.out 2-public-02.out
+./14 < 2-public-03.in > my_2_public_03.out
+diff my_2_public_03.out 2-public-03.out
+./14 < 2-public-04.in > my_2_public_04.out
+diff my_2_public_04.out 2-public-04.out
+./14 < 2-public-05.in > my_2_public_05.out
+diff my_2_public_05.out 2-public-05.out
+
+./14 < 3-public-01.in > my_3_public_01.out
+diff my_3_public_01.out 3-public-01.out
+./14 < 3-public-02.in > my_3_public_02.out
+diff my_3_public_02.out 3-public-02.out
+./14 < 3-public-03.in > my_3_public_03.out
+diff my_3_public_03.out 3-public-03.out
+./14 < 3-public-04.in > my_3_public_04.out
+diff my_3_public_04.out 3-public-04.out
+./14 < 3-public-05.in > my_3_public_05.out
+diff my_3_public_05.out 3-public-05.out
+./14 < 3-public-06.in > my_3_public_06.out
+diff my_3_public_06.out 3-public-06.out
+
+./14 < 4-public-01.in > my_4_public_01.out
+diff my_4_public_01.out 4-public-01.out
+./14 < 4-public-02.in > my_4_public_02.out
+diff my_4_public_02.out 4-public-02.out
+./14 < 4-public-03.in > my_4_public_03.out
+diff my_4_public_03.out 4-public-03.out
+./14 < 4-public-04.in > my_4_public_04.out
+diff my_4_public_04.out 4-public-04.out
+./14 < 4-public-05.in > my_4_public_05.out
+diff my_4_public_05.out 4-public-05.out
+./14 < 4-public-06.in > my_4_public_06.out
+diff my_4_public_06.out 4-public-06.out
