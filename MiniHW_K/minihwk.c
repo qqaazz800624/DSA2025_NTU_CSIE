ec3f175e1cb3598de378b77454448583debf4090
qqaazz800624 <qqaazz800624@gmail.com>
2025-05-26 16:38:27 +0800

    update RB tree

--- MiniHW_K/minihwk.c
+++ MiniHW_K/minihwk.c
@@ -10,13 +10,14 @@ typedef struct {
 } Node;
 
 static Node *T;
-static int   root;
+static int root;
 
 typedef struct { 
     int key, idx; 
 } Pair;
+
 static Pair *mapArr;
-static int   mapSize;
+static int mapSize;
 
 int cmp_pair(const void *a, const void *b);
 int map_get(int key);
@@ -49,8 +50,9 @@ int main(void)
             stk[++top] = i;
         } else {
             int last = NIL;
-            while (top >= 0 && k > T[stk[top]].key)
+            while (top >= 0 && k > T[stk[top]].key){
                 last = stk[top--];
+            }
             T[last].right = i;
             T[i].parent = last;
             stk[++top] = i;
@@ -60,8 +62,10 @@ int main(void)
 
     mapArr  = (Pair*)malloc(sizeof(Pair) * N);
     mapSize = N;
-    for (int i = 0; i < N; ++i)
-        mapArr[i].key = T[i].key, mapArr[i].idx = i;
+    for (int i = 0; i < N; ++i){
+        mapArr[i].key = T[i].key;
+        mapArr[i].idx = i;
+    }
     qsort(mapArr, N, sizeof(Pair), cmp_pair);
 
     int M;  
@@ -75,9 +79,18 @@ int main(void)
             left_rotate(idx);
         } else if (op == 'R') {
             right_rotate(idx);
-        } else {                      
-            int LK = (T[idx].left  == NIL) ? -1 : T[T[idx].left ].key;
-            int RK = (T[idx].right == NIL) ? -1 : T[T[idx].right].key;
+        } else {   
+            int LK, RK;
+            if (T[idx].left == NIL) {
+                LK = -1;
+            } else {
+                LK = T[T[idx].left].key;
+            }
+            if (T[idx].right == NIL) {
+                RK = -1;
+            } else {
+                RK = T[T[idx].right].key;
+            }                   
 
             int depth = 0, bdepth = 0;
             for (int p = T[idx].parent; p != NIL; p = T[p].parent) {
@@ -102,9 +115,14 @@ int map_get(int key) {
     int lo = 0, hi = mapSize - 1;
     while (lo <= hi) {
         int mid = (lo + hi) >> 1;
-        if (mapArr[mid].key == key) return mapArr[mid].idx;
-        if (key < mapArr[mid].key)   hi  = mid - 1;
-        else                         lo  = mid + 1;
+        if (mapArr[mid].key == key){
+            return mapArr[mid].idx;
+        }
+        if (key < mapArr[mid].key){
+            hi  = mid - 1;
+        } else {
+            lo  = mid + 1;
+        }
     }
     return NIL;                      
 }
@@ -112,12 +130,18 @@ int map_get(int key) {
 void left_rotate(int x) {
     int y = T[x].right;                 
     T[x].right = T[y].left;
-    if (T[y].left != NIL) T[T[y].left].parent = x;
+    if (T[y].left != NIL){
+        T[T[y].left].parent = x;
+    } 
 
     T[y].parent = T[x].parent;
-    if (T[x].parent == NIL)                root = y;
-    else if (x == T[T[x].parent].left)     T[T[x].parent].left  = y;
-    else                                   T[T[x].parent].right = y;
+    if (T[x].parent == NIL){
+        root = y;
+    } else if (x == T[T[x].parent].left) {
+        T[T[x].parent].left  = y;
+    } else {
+        T[T[x].parent].right = y;
+    }
 
     T[y].left = x;
     T[x].parent = y;
@@ -126,12 +150,18 @@ void left_rotate(int x) {
 void right_rotate(int y) {
     int x = T[y].left;
     T[y].left = T[x].right;
-    if (T[x].right != NIL) T[T[x].right].parent = y;
+    if (T[x].right != NIL){
+        T[T[x].right].parent = y;
+    }
 
     T[x].parent = T[y].parent;
-    if (T[y].parent == NIL)                root = x;
-    else if (y == T[T[y].parent].left)     T[T[y].parent].left  = x;
-    else                                   T[T[y].parent].right = x;
+    if (T[y].parent == NIL){
+        root = x;
+    } else if (y == T[T[y].parent].left){
+        T[T[y].parent].left  = x;
+    } else{
+        T[T[y].parent].right = x;
+    }
 
     T[x].right = y;
     T[y].parent = x;
